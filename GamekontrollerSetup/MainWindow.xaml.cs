782778a51eb71c03bdcc62126963a5af4a8f1b2f
Stefine <stefine.varde@gmail.com>
2021-11-08 12:57:40 +0100

    Add project files.

---
+++ GamekontrollerSetup/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GamekontrollerSetup
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sensorB_Click(object sender, RoutedEventArgs e)
+        {
+            SensorWindow sensorW = new SensorWindow();
+            sensorW.Show();
+        }
+    }
+}
---
+++ GamekontrollerSetup/SensorWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GamekontrollerSetup
+{
+    /// <summary>
+    /// Interaction logic for SensorWindow.xaml
+    /// </summary>
+    public partial class SensorWindow : Window
+    {
+        public SensorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void backB_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
